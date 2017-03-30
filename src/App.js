d86190d613369eea607d40ea075752e6c8dfb52d
Carl Schmidt <carl.schmidt@gmail.com>
2017-03-30 13:49:08 -0700

    Trying a diff approach to state structure, working towards getting Redux updates going

--- src/App.js
+++ src/App.js
@@ -22,17 +22,26 @@ const actions = {
 }
 
 const initialGameState = {
-  rows: [["", "", ""], ["", "", ""], ["","",""]],
+  squares: {
+    a1: "",
+    a2: "",
+    a3: "",
+    b1: "",
+    b2: "",
+    b3: "",
+    c1: "",
+    c2: "",
+    c3: ""
+  },
   turn: players.X,
   outcome: outcomes.UNKNOWN
 }
 
 
 // Actions
-const makeMove = (rowNum, colNum) => ({
+const makeMove = (squareKey) => ({
   type:actions.MAKE_MOVE,
-  row: rowNum,
-  col: colNum})
+  squareKey: squareKey})
 
 
 // Reducers
@@ -40,10 +49,10 @@ const move = (game = {}, action) => {
   switch (action.type) {
     case actions.MAKE_MOVE :
       var newGameState = Object.assign({}, game)
-      var isSquareEmpty = newGameState.rows[action.row][action.col] === ""
+      var isSquareEmpty = newGameState.squares[action.squareKey] === ""
       // mark the game board (if the requested square is empty)
       if (isSquareEmpty) {
-        newGameState.rows[action.row][action.col] = game.turn
+        newGameState.squares[action.squareKey] = game.turn
         // switch players
         newGameState.turn = (game.turn === players.X ? players.O : players.X)
       }
@@ -61,33 +70,41 @@ function ticTacToe(state, action) {
   return move(state, action)
 }
 
+
 const store = createStore(ticTacToe, initialGameState)
+
 
 // Components
 
-const Square = (square, rowNum, colNum) => {
-  const dispatch = (event) => {
-    store.dispatch(makeMove(rowNum, colNum))
-  }
-  return React.createElement("span",
-                      {className: "ticTacToeSquare",
-                       key: rowNum.toString() + "-" + colNum.toString(),
-                       onClick: dispatch},
-                       square)
+const Square = ({onClick, value}) => {
+  return (
+    <span className="ticTacToeSquare" onClick={onClick}>{value}</span>
+  )
 }
 
-const Row = (row, rowNum) =>
-  <div className="ticTacToeRow" key={rowNum}>
-    {row.map((square, colNum) => Square(square, rowNum, colNum))}
-  </div>
+Square.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.string
+}
 
-const Board = ({game}) =>
+const Board = ({game}) => {
+  return (
   <div>
     <h1>Tic Tac Toe</h1>
     <div className="ticTacToeBoard">
-      {game.rows.map((row, rowNum) => Row(row,rowNum))}
+      {[1, 2, 3].map( (row) =>
+        <div key={row}>
+          {["a", "b", "c"].map((col) =>
+            <Square key={col+row}
+                onClick={() => store.dispatch(makeMove(col+row))}
+                value={game.squares[col+row]} />
+            )}
+        </div>
+      )}
     </div>
   </div>
+  )
+}
 
 Board.propTypes = {
   game: PropTypes.object.isRequired
